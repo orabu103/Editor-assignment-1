b4925fcc1ac73e8c039c411fa47e713833bd57af
Or Abuhtzura <44296674+orabu103@users.noreply.github.com>
2019-11-08 19:20:33 +0200

    Update Document.cpp

--- Document.cpp
+++ Document.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 #include "Document.h"
+#include <algorithm>
 using namespace std;
 
 void Document::p(){  // prints the current line (ed maintains a current line)
-    int size = vec.size();
-    cout<<vec[size-1]<<endl;
+    cout<<vec[pos-1]<<endl;
 }
 void Document::n(){ // prints line number of current line followed by TAB followed by current line
-    int size = vec.size();
-    cout<<size <<"  "<< vec[size-1] << endl;
+    cout<<pos <<"  "<< vec[pos-1] << endl;
 }
 void Document::a(){ //appends new text after the current line
     string str;
     cin.ignore();
     getline(cin, str);
     while(str!="."){
-        vec.push_back(str);
+        pos++;
+        vec.insert(vec.begin()+pos-1,str);
         getline (cin, str);
     }
 }
@@ -28,33 +28,58 @@ void Document::i(){ //inserts new text before the current line
     cin.ignore();
     getline(cin, str);
     while(str!="."){
+        pos++;
         vec.insert(vec.end()-1,str);
         getline(cin, str);
     }
 } 
 void Document::c(){ //changes the current line for text that follows
-    vec.pop_back();
+    vec.erase(vec.begin()+(pos-1));
     string str;
     cin.ignore();
     getline(cin, str);
     while(str!="."){
-        vec.push_back(str);
+        vec.insert(vec.begin()+pos-1,str);
         getline (cin, str);
     }
 }
 void Document::d(){ //deletes the current line
-    if(vec.size()>=1)
-    vec.pop_back();
-}
-void Document::search(string str){ //searches forward after current line for the specified text
-    for(int i=0; i <vec.size(); i++){
-
+    if(vec.size()>=1){
+    vec.erase(vec.begin()+(pos-1));
+    pos--;
     }
+
 }
-void Document::repeatSrearch(string str){} //Repeat search
-void Document::backward(){}//searches backward
 void Document::number(string str){ //makes line #number the current line
     pos = stoi(str);
+    cout << vec[pos-1] << endl;
 }
-void Document::replaces(){} //replaces old string with new in current line 
+void Document::search(string str){ //searches forward after current line for the specified text
+         for(int i=pos; i < vec.size(); i++){
+             string ans = vec[i];
+             if(ans.find(str)!= std::string::npos){
+                cout << vec[i] << endl;
+                pos=i+1;
+                break;
+             }
+         }
+           
+}
+void Document::repeatSrearch(string str){ //Repeat search
+    Document::search(str);
+}
+void Document::backward(string str){//searches backward
+    for(int i=pos-1; i >= 0; i--){
+             string ans = vec[i];
+             if(ans.find(str)!= std::string::npos){
+                cout << vec[i] << endl;
+                pos=i+1;
+                break;
+             }
+         }
+}
+
+void Document::replaces(){ //replaces old string with new in current line 
+
+}
 
